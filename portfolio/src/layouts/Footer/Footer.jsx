a6510d7a0a601cdbcfa2b2ed37c89dfa9b0dbe13
cardon-a <alexis.cardon@hotmail.fr>
2023-07-10 08:43:47 +0200

    Correction d'erreur de code dans le fichier "Footer.jsx"

--- portfolio/src/layouts/Footer/Footer.jsx
+++ portfolio/src/layouts/Footer/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
         <footer>
             <p>CARDON Alexis</p>
             <div>
-                <p><i class="fa-solid fa-phone icon"></i> 06.27.91.55.05</p>
-                <p><i class="fa-solid fa-location-dot icon"></i> Normandie, France</p>
-                <p><i class="fa-solid fa-at icon"></i> alexis.cardon@hotmail.fr</p>
+                <p><i className="fa-solid fa-phone icon"></i> 06.27.91.55.05</p>
+                <p><i className="fa-solid fa-location-dot icon"></i> Normandie, France</p>
+                <p><i className="fa-solid fa-at icon"></i> alexis.cardon@hotmail.fr</p>
             </div>
         </footer>
     )
